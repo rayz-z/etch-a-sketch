0e2a8976537b0dcf0012812804af6359df8d8492
Raymond <raymond.z1859@gmail.com>
2024-12-24 20:23:40 -0600

    added a button to set dimensions

--- etch.js
+++ etch.js
@@ -1,5 +1,9 @@
 const container = document.querySelector("div");
+const squaresBtn = document.querySelector("button");
 
+squaresBtn.addEventListener("click", () => {
+    setSquares();
+});
 
 for (let i = 0; i < 16; i++){
     const i = document.createElement("div");
@@ -26,4 +30,40 @@ for (let i = 0; i < 16; i++){
     }
     
 }
+
+function setSquares() {
+    const squares = prompt("How many squares per side would you like? ");
+    if (squares > 100) {
+        alert("Dimensions too large. Please input a number smaller than 100.");
+    }
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < squares; i++){
+        const i = document.createElement("div");
+        container.appendChild(i);
+    
+        i.style.border = "2px solid red";
+        i.style.width = "auto";
+        i.style.height = "auto";
+        i.style.display = "flex";
+        
+        for (let j = 0; j < squares; j++) {
+            const j = document.createElement("div");
+            i.appendChild(j);
+    
+            j.style.border = "2px solid green";
+            j.style.width = "auto";
+            j.style.height = "30px";
+            j.style.display = "flex";
+            j.style.flex = 1;
+    
+            j.addEventListener("mouseover", () => {
+                j.style.backgroundColor = "gray";
+            });
+        }
+        
+    }
+}
+
 
